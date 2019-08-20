8efc9ea5b20ca3ee70612aa88441225641fa9e86
dnguyen800 <25126347+dnguyen800@users.noreply.github.com>
2019-08-19 18:12:37 -0700

    Revert "Update to mirror https://github.com/bouwew/hassio-addons-1"
    This reverts commit a7b5899a3d0b3f327ddd62b3ad4040f7c38d57e9.

--- magic_mirror/config.js
+++ magic_mirror/config.js
@@ -1,205 +1,87 @@
-/* Magic Mirror Config for HASS.IOSample
+/* Magic Mirror Config Sample for version 2.8.0
  *
- * Orginal by Michael Teeuw http://michaelteeuw.nl
+ * By Michael Teeuw http://michaelteeuw.nl
  * MIT Licensed.
  *
  * For more information how you can configurate this file
  * See https://github.com/MichMich/MagicMirror#configuration
  *
- * When running in HASS.IO the address and ipWhitelist need to be 
- * empty and any access mcontrols need to be on the host
- * and not in the container.
  */
 
 var config = {
+	// Hass.io specific settings
     address: "", // Empty as running in a docker container.
     port: 8080, // Update port in HASS.IO configuration and not here.
     ipWhitelist: [], // Empty as in docker container, ACL from the host.
-
+	
+	
     //
     // The settings below are for a basic setup, please modifiy as needed. 
-    //
-    language: "en",
-    timeFormat: 24,
-    units: "imperial",
-    customCss: "css/custom.css",
+    //	
+	language: "en",
+	timeFormat: 24,
+	units: "imperial",
 
-    modules: [
-        {
-            module: "alert",
-        },
-        {
-            module: "updatenotification",
-            position: "top_bar"
-        },
-        {
-            module: 'MMM-OnScreenMenu',
-            position: 'bottom_right',
-            config: {
-                touchMode: true,
-                enableKeyboard: true,
-                menuItems: {
-                    notify1: {  title: "Home", 
-                        icon: "eye", 
-                        notification: "PAGE_CHANGED", 
-                        payload: 0 },
-                    notify2: {  title: "Comics", 
-                        icon: "eye", 
-                        notification: "PAGE_CHANGED", 
-                        payload: 1 },
-                    notify3: {  title: "Home Assistant", 
-                        icon: "eye", 
-                        notification: "PAGE_CHANGED", 
-                        payload: 2 }
-                }
-            }
-        },
-        {
-            module: "MMM-pages",
-            config: {
-                modules: [
-                        [ "calendar", "clock","weatherforecast","currentweather","newsfeed", "MMM-HASS", "MMM-homeassistant-sensors" ],
-                        [ "DailyXKCD" ],
-                        [ "MMM-iFrame" ]
-                ],
-                excludes: ["alert", "updatenotification", "MMM-OnScreenMenu"],
-            }            
-        },    
-        {
-            module: 'MMM-iFrame',
-            position: 'fullscreen_below',	// This can be any of the regions.
-            config: {
-                    url: ["http://hass.local:8123"],
-                    updateInterval: 60 * 60 * 1000,// Refresh every 60 minutes
-                    width: "100%",
-                    height: "1000px"
-                }
-        },
-        {
-            module: "DailyXKCD",
-            position: 'top_left',
-            config: {
-                invertColors: true,
-                showTitle: true,
-                showAltText: false
-            }            
-        },
-        {
-            module: "clock",
-            position: "top_left"
-        },
-        {
-            module: "calendar",
-            header: "US Holidays",
-            position: "top_left",
-            config: {
-                calendars: [
-                    {
-                        symbol: "calendar-check-o ",
-                        url: "webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics"
-                    }
-                ]
-            }
-        },
-        {
-            module: "compliments",
-            position: "lower_third"
-        },
-        {
-            module: "currentweather",
-            position: "top_right",
-            config: {
-                location: "",
-                locationID: "",  //ID from http://www.openweathermap.org/help/city_list.txt
-                appid: ""
-            }
-        },
-        {
-            module: "weatherforecast",
-            position: "top_right",
-            header: "Weather Forecast",
-            config: {
-                location: "",
-                locationID: "",  //ID from http://www.openweathermap.org/help/city_list.txt
-                appid: ""
-            }
-        },
-        {
-            module: "newsfeed",
-            position: "bottom_bar",
-            config: {
-                feeds: [
-                    {
-                        title: "New York Times",
-                        url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-                    }
-                ],
-                showSourceTitle: true,
-                showPublishDate: true
-            }
-        },
-        {
-                module: "MMM-HASS",
-                position: "top_left",
-                config: {
-                        host: "hassio/homeassistant", // Special docker ha api proxy
-                        port: "",
-                        https: false,
-			hassiotoken: true,
-                        devices: [
-                        { deviceLabel: "Exterior",
-                                deviceReadings: [
-                                { sensor: "sensor.dark_sky_temperature", icon: "wi wi-thermometer", suffix: "°"},
-                                { sensor: "sensor.dark_sky_humidity", icon: "wi wi-humidity", suffix: "%"}
-                                ]
-                        },
-                        { deviceLabel: "Interior",
-                                deviceReadings: [
-                                { sensor: "sensor.main_floor_temperature", icon: "wi wi-thermometer", suffix: "°", notification: "INDOOR_TEMPERATURE"},
-                                { sensor: "sensor.main_floor_humidity", icon: "wi wi-humidity", suffix: "%"}
-                                ]
-                        },
-                        { deviceLabel: "Internet",
-                                deviceReadings: [
-                                { sensor: "sensor.speedtest_ping", icon: "fa fa-tachometer-alt", suffix: ""}
-                                ]
-                        }
-                        ]
-                  }
-        },        
-        {
-		    module: 'MMM-homeassistant-sensors',
-		    position: 'top_left',
-		    config: {
-			    url: 'http://hassio/homeassistant/api/states', // Special docker ha api proxy
-			    prettyName: false,
-			    stripName: false,
-			    values: [{
-					sensor: "sensor.processor_use",
-					icons: [{
-							"default": "chip"
-						}
-					]
-				}, {
-					sensor: "binary_sensor.downstairs_occupancy",
-					icons: [{
-							"state_off": "run",
-							"state_on": "run-fast"
-						}
-					]
-				}, {
-					sensor: "switch.study",
-					icons: [{
-							"state_off": "lightbulb-outline",
-							"state_on": "lightbulb-on-outline"
-						}
-					]
-				}
-			]
-
-		}
-	}        
-    ]
+	modules: [
+		{
+			module: "alert",
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+		{
+			module: "clock",
+			position: "top_left"
+		},
+		{
+			module: "calendar",
+			header: "US Holidays",
+			position: "top_left",
+			config: {
+				calendars: [
+					{
+						symbol: "calendar-check",
+						url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics"					}
+				]
+			}
+		},
+		{
+			module: "currentweather",
+			position: "top_right",
+			config: {
+				location: "Milpitas",
+				locationID: "5373327",  // Go to https://openweathermap.org/find , search for your city, and copy the 7-digit number in the URL. For example, Milpitas is https://openweathermap.org/city/5373327
+				appid: "YOUR_OPENWEATHER_API_KEY" // Create an account and obtain key from https://openweathermap.org/
+			}
+		},
+		{
+			module: "weatherforecast",
+			position: "top_right",
+			header: "Weather Forecast",
+			config: {
+				location: "Milpitas",
+				locationID: "5373327",  // Go to https://openweathermap.org/find , search for your city, and copy the 7-digit number in the URL. For example, Milpitas is https://openweathermap.org/city/5373327
+				appid: "YOUR_OPENWEATHER_API_KEY" // Create an account and obtain key from https://openweathermap.org/
+			}
+		},
+		{
+			module: "newsfeed",
+			position: "bottom_bar",
+			config: {
+				feeds: [
+					{
+						title: "New York Times",
+						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true,
+				broadcastNewsFeeds: true,
+				broadcastNewsUpdates: true
+			}
+		},
+	]
 
 };
 
